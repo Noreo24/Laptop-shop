5daa3711238047c624746a74caecb6838d460c8a
Noreo24 <buianhtuan.kbhb24@gmail.com>
2025-02-03 20:46:49 +0700

    edit

--- src/main/java/vn/noreo/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/noreo/laptopshop/LaptopshopApplication.java
@@ -1,13 +1,13 @@
-package vn.noreo.java_spring_laptopshop_final_project_1;
+package vn.noreo.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class LaptopshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(LaptopshopApplication.class, args);
 	}
 
 }
