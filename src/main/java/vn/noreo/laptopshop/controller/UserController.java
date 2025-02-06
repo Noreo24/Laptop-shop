523d40cc620321987f952c307418cd6b32a835dc
Noreo24 <buianhtuan.kbhb24@gmail.com>
2025-02-06 12:28:03 +0700

    12h27 SA - 06/02/2025

---
+++ src/main/java/vn/noreo/laptopshop/controller/UserController.java
@@ -0,0 +1,37 @@
+package vn.noreo.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.noreo.laptopshop.service.UserService;
+
+// Mô hình MVC
+// @Controller
+// public class UserController {
+
+//     @RequestMapping("/")
+//     public String getHomePage() {
+//         return "Hello from controller";
+//     }
+// }
+
+// Mô hình restful API
+@RestController
+public class UserController {
+
+    // DI: Dependency injection
+    private UserService userService;
+
+    // Không dùng AutoWired vì không tốt cho test
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // Gọi qua Model
+    @GetMapping("/")
+    public String getHomePage() {
+        return this.userService.handleHello();
+    }
+}
---
+++ src/main/java/vn/noreo/laptopshop/service/UserService.java
@@ -0,0 +1,10 @@
+package vn.noreo.laptopshop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public String handleHello() {
+        return "Hello from service";
+    }
+}
