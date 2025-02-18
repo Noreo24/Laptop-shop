91ddafd4c549f79e11d66c49d4fe3a68e93142c8
Noreo24 <buianhtuan.kbhb24@gmail.com>
2025-02-18 11:22:45 +0700

    11h20 SA - 18/02/2025

---
+++ src/main/java/vn/noreo/laptopshop/controller/client/HomepageController.java
@@ -0,0 +1,13 @@
+package vn.noreo.laptopshop.controller.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomepageController {
+
+    @GetMapping("/")
+    public String getHomepage() {
+        return "client/homepage/homepage";
+    }
+}
