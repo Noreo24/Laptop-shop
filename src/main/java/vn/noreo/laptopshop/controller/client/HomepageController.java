c6457d2f2befa0a8b8f5f49efe254e7788f3314d
Noreo24 <buianhtuan.kbhb24@gmail.com>
2025-02-24 02:23:34 +0700

    2h23 SA - 24/02/2025

--- src/main/java/vn/noreo/laptopshop/controller/client/HomepageController.java
+++ src/main/java/vn/noreo/laptopshop/controller/client/HomepageController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import vn.noreo.laptopshop.domain.Product;
+import vn.noreo.laptopshop.domain.dto.RegisterDTO;
 import vn.noreo.laptopshop.service.ProductService;
 
 @Controller
@@ -23,5 +26,17 @@ public class HomepageController {
         List<Product> allProducts = this.productService.getAllProducts();
         model.addAttribute("allProducts", allProducts);
         return "client/homepage/homepage";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage(Model model) {
+        model.addAttribute("registerUser", new RegisterDTO());
+        return "client/auth/register";
+    }
+
+    @PostMapping("/register")
+    public String handleRegister(@ModelAttribute("registerUser") RegisterDTO registerDTO) {
+
+        return "client/auth/register";
     }
 }
